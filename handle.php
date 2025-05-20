24a3fea60d12d519aeda1d8098bd06de0b1314a7
louisnguyen2005 <kinguyen291118@gmail.com>
2025-05-20 21:58:29 +0700

    delete

--- handle.php
+++
@@ -1,8 +0,0 @@
-<form action="handle.php" method="get">
-    <input type="text" name="post_data">
-    <button type="submit">click on</button>
-
-</form>
-<?php
-        echo $_GET["post_data"];
-?>
